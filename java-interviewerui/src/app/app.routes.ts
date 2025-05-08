a97e1bc97226780491faadd49154509fa820fb59
ENT-00180 <ent-00180@ENT-00180s-MacBook-Air.local>
2025-05-08 13:35:05 +0530

    app.routes.ts

--- java-interviewerui/src/app/app.routes.ts
+++ java-interviewerui/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { InterviewerComponent } from './interviewer/interviewer.component';
 import { ThankYouComponent } from './thank-you/thank-you.component';
 import { InstructionsComponent } from './instructions/instructions.component';
 import { InterviewOptionsComponent } from './interview-options/interview-options.component';
+// import { register } from 'module';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path:'register',component:RegisterComponent},
   { path: 'login', component: LoginComponent },
   { path: 'options', component: InterviewOptionsComponent },
   { path: 'instructions', component: InstructionsComponent },
